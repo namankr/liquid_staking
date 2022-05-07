492c5aee5ba69bf32107813bed823891c1beeb04
Naman <naman@workplacefabric.com>
2022-05-07 16:06:10 +0530

    //to do add coolDown functions

--- src/contracts/DecentralBank.sol
+++ src/contracts/DecentralBank.sol
@@ -16,4 +16,62 @@ contract DecentralBank {
         stkzamp = _stkzamp;
     }
 
+    address[] public stakers;
+    mapping(address => uint) public stakingBalance;
+    mapping(address => bool) public hasStaked;
+    mapping(address => bool) public isStaking;
+
+    //staking functions
+    function depositTokens(uint _amount) public {
+        //require Staking amount cannot be Zero
+        require(_amount>0, 'amount cannot be zero');
+
+        // Transfer zamp tokens to this contract address for staking
+        zamp.transferFrom(msg.sender, address(this), _amount);
+    
+        //update Staking Balance
+        stakingBalance[msg.sender] = stakingBalance[msg.sender] +_amount;
+
+        if(!hasStaked[msg.sender]){
+            stakers.push(msg.sender);
+        }
+
+        //Update Staking Balance
+
+        isStaking[msg.sender] = true;
+        hasStaked[msg.sender] = true;
+
+    }
+
+    function issueTokens() public {
+
+        require(msg.sender == owner, 'only owner can issue rewards');
+
+        for(uint i=0; i<stakers.length; i++){
+            address recipient = stakers[i];
+            uint balance = stakingBalance[recipient]/9; // div by 9 to create percentage incentives
+            if(balance>0){
+                stkzamp.transfer(recipient, balance);
+            }
+        }
+    }
+
+    function claim() public {
+
+        uint balance = stakingBalance[msg.sender];
+        
+
+        require(balance>0 , 'staking balance cannot be less than zero');
+
+        //transfer the tokens to the specified address from contract
+        zamp.transfer(msg.sender, balance);
+        
+        // reset the staking balance
+        stakingBalance[msg.sender] =0;
+
+        //update the Staking status
+        isStaking[msg.sender] = false;
+
+    }
+
 }
--- test/decentralBank.test.js
+++ test/decentralBank.test.js
@@ -51,6 +51,56 @@ contract('DecentralBank', ([owner, customer]) => {
             let balance = await reward.balanceOf(decentralBank.address)
             assert.equal(balance, tokens('1000000'))
         })
+
+        describe('Yeild Farming', async () => {
+            it('rewards token for staking', async() =>{
+                let result;
+                
+                // check investor balance
+                result = await zamp.balanceOf(customer)
+                assert.equal(result.toString(), tokens('100'), 'customer wallet balance' )
+            
+                //check Staking For Customer of 100 tokens
+                await zamp.approve(decentralBank.address, tokens('100'), {from:customer})
+                await decentralBank.depositTokens(tokens('100'), {from:customer})
+
+                //check Updated Balance of Customer
+                result = await zamp.balanceOf(customer)
+                assert.equal(result.toString(), tokens('0'), 'customer wallet balance after ')
+
+                //check Updated Balance of Decentral Bank
+                result = await zamp.balanceOf(decentralBank.address)
+                assert.equal(result.toString(), tokens('100'), 'decentral bank wallet balance after staking from customer')
+
+
+                //Is Staking Balance
+                result = await decentralBank.isStaking(customer)
+                assert.equal(result.toString(), 'true', 'customer is staking status after')
+
+                //Issue tokens
+                await decentralBank.issueTokens({from: owner})
+
+                //Ensure only the Owner can issue tokens 
+                 //await decentralBank.issueTokens({from:owner})
+
+
+                //claim tokens 
+                await decentralBank.claim({from: customer})
+
+                //check claim balance
+                result = await zamp.balanceOf(customer)
+                assert.equal(result.toString(), tokens('100'), 'customer wallet balance after claim ')
+
+                // check updated balance of Decentral bank
+                result = await zamp.balanceOf(decentralBank.address)
+                assert.equal(result.toString(), tokens('0'), 'decentral bank wallet balance after staking from customer')
+
+                //Is Staking 
+                result = await decentralBank.isStaking(customer)
+                assert.equal(result.toString(), 'false', 'customer is staking status after')
+
+            })
+        })  
     })
 
 })
