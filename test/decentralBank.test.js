99c367f1e8fd4c1d9aa2760c195e773a1aa1e0ae
Naman <naman@workplacefabric.com>
2022-05-07 10:17:34 +0530

    added test for decentral bank

--- test/decentralBank.test.js
+++ test/decentralBank.test.js
@@ -6,24 +6,50 @@ require('chai')
 .use(require('chai-as-promised'))
 .should()
 
-contract('DecentralBank', (accounts) => {
-    let zamp, reward;
+contract('DecentralBank', ([owner, customer]) => {
+    let zamp, reward, decentralBank;
+
+    function tokens(number){
+        return web3.utils.toWei(number, 'ether')
+    }
+
     before(async ()=>{
+        // load contracts
         zamp = await Zamp.new()
         reward = await stkZamp.new()
+        decentralBank = await DecentralBank.new(zamp.address, reward.address) 
+
+        // transfer all token to DecentralBank (1 million)
+        await reward.transfer(decentralBank.address, tokens('1000000'))
+
+        // transfer 100 Zamp tokens to customer
+        await zamp.transfer(customer, tokens('100'), {from:owner})
+
     })
     
-    describe('Fictional Zamp Deployment', async() =>{
+    describe('Fictional Zamp token Deployment', async() =>{
         it('matches name successfully', async () =>{
             const name = await zamp.name()
             assert.equal(name, 'Zamp')
         })
     })
 
-    describe('Reward Token', async() =>{
+    describe(' stkZamp Reward Token', async() =>{
         it('matches name successfully', async () =>{
             const name = await reward.name()
             assert.equal(name, 'stkZamp')
+        })
+    })
+
+    describe('Decentral Bank Deployment', async() =>{
+        it('matches name successfully', async () =>{
+            const name = await decentralBank.name()
+            assert.equal(name, 'Decentral Bank')
+        })
+
+        it('contract has tokens', async () =>{
+            let balance = await reward.balanceOf(decentralBank.address)
+            assert.equal(balance, tokens('1000000'))
         })
     })
 
